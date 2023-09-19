3a05c84faf1e1b92e733e471ebfb4a113b179788
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-09-19 16:44:40 +0300

    Update Nav.js
    Update Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,2 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/nav.css';
+
+const Nav = () => (
+  <nav id="navbar">
+    <ul>
+      <li>
+        <NavLink to="/" exact activeClassName="active">
+          Rockets
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/missions" activeClassName="active">
+          Missions
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/myprofile" activeClassName="active">
+          My Profile
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
