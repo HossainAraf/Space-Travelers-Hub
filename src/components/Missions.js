3a977dbcd6763abcc7850635c3ba072eebbc5bc6
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-09-20 10:54:36 +0300

    Update Missions.js
    Add more state handling statement

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -7,10 +7,33 @@ import { getMissions } from '../redux/missionsSlice';
 const Missions = () => {
   const dispatch = useDispatch();
   const missions = useSelector((state) => state.missions.missions);
-  console.log('Missions:', missions);
+  //console.log('Missions:', missions);
 
   // FETCH MISSIONS DATA ON PAGE LOAD
   useEffect(() => {
     dispatch(getMissions());
   }, [dispatch]);
-  
+  // SHOW LOADING, ERROR, OR MISSIONS DATA
+  const renderMissions = () => {
+    if (missions.length === 0) {
+      return <p>Loading missions...</p>;
+    }
+
+    return missions.map((mission) => (
+      <div key={mission.id}>
+        <h2>{mission.name}</h2>
+        <p>{mission.description}</p>
+      </div>
+    ));
+  };
+
+  return (
+    <section>
+      <h1>Missions</h1>
+      {renderMissions()}
+    </section>
+  );
+};
+
+// EXPORT
+export default Missions;
