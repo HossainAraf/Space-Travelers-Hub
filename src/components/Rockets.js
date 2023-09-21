d92e490cc14c5b823849220b89e81263dc807c25
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-09-21 12:52:12 +0300

    updated the rocket.js
    updated the rocket.js file

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -3,24 +3,44 @@ import { useEffect } from 'react';
 import { getRockets, reserveRocket, cancelRocket } from '../redux/rocketsSlice';
 import { ReserveRocketButton, CancelRocketButton } from './Button';
 import '../styles/rockets.css';
+// SHOW LOADING, ERROR, OR ROCKETS DATA
+const renderRockets = () => {
+  if (rockets.length === 0) {
+    return <p>Loading rockets...</p>;
+  }
 
-// GET ROCKETS DATA FROM REDUX STORE
-const Rockets = () => {
-  const dispatch = useDispatch();
-  const rockets = useSelector((state) => state.rockets.rockets);
-  console.log('Rockets:', rockets);
 
-  // FETCH ROCKETS DATA ON PAGE LOAD
-  useEffect(() => {
-    dispatch(getRockets());
-  }, [dispatch]);
-
-  // RESERVE/CANCEL RESERVATION OF ROCKETS
-  const HandleReserveRocket = (id, reserved) => {
-    if (reserved) {
-      dispatch(cancelRocket(id));
-    } else {
-      dispatch(reserveRocket(id));
-    }
-    console.log('HandleReserveRocket:', id, reserved);
-  };
+  return rockets.map((rocket) => (
+    <div className="container-rocket" key={rocket.id}>
+      <h2>{rocket.name}</h2>
+      <h3>{rocket.reserved ? 'Reserved' : ''}</h3>
+      <div className="btn-rocket">
+        {rocket.reserved ? (
+          <CancelRocketButton
+            onClick={() => HandleReserveRocket(rocket.id, rocket.reserved)}
+          >
+            Cancel
+          </CancelRocketButton>
+        ) : (
+          <ReserveRocketButton
+            onClick={() => HandleReserveRocket(rocket.id, rocket.reserved)}
+          >
+            Reserve
+          </ReserveRocketButton>
+        )}
+      </div>
+      <p>{rocket.description}</p>
+      <div className="img-rocket">
+        <img src={rocket.image} alt={rocket.image} />
+      </div>
+    </div>
+  ));
+};
+return (
+  <section>
+    <h1>Rockets</h1>
+    {renderRockets(HandleReserveRocket)}
+  </section>
+);
+};
+export default Rockets;
