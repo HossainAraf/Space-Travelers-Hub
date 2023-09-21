aed8fc8e5a79a0d3f50d93b53b75fdebba02185e
Hossain <arafat.kd99@gmail.com>
2023-09-21 11:29:54 +0100

    Update Reockets component to fix error and render properly

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,46 +1,56 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getRockets, reserveRocket, cancelRocket } from '../redux/rocketsSlice';
-import { ReserveRocketButton, CancelRocketButton } from './Button';
 import '../styles/rockets.css';
-// SHOW LOADING, ERROR, OR ROCKETS DATA
-const renderRockets = () => {
-  if (rockets.length === 0) {
-    return <p>Loading rockets...</p>;
-  }
 
+// GET ROCKETS DATA FROM REDUX STORE
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rockets);
+  console.log('Rockets:', rockets);
 
-  return rockets.map((rocket) => (
-    <div className="container-rocket" key={rocket.id}>
-      <h2>{rocket.name}</h2>
-      <h3>{rocket.reserved ? 'Reserved' : ''}</h3>
-      <div className="btn-rocket">
+  // FETCH ROCKETS DATA ON PAGE LOAD
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
+
+  const handleReserveRocket = (id, reserved) => {
+    if (reserved) {
+      dispatch(cancelRocket(id));
+    } else {
+      dispatch(reserveRocket(id));
+    }
+  };
+
+  // SHOW LOADING, ERROR, OR ROCKETS DATA
+  const renderRockets = () => {
+    if (rockets.length === 0) {
+      return <p>Loading rockets...</p>;
+    }
+
+    return rockets.map((rocket) => (
+      <div className="container-rocket" key={rocket.id}>
+        <h2>{rocket.name}</h2>
+        <p>{rocket.description}</p>
         {rocket.reserved ? (
-          <CancelRocketButton
-            onClick={() => HandleReserveRocket(rocket.id, rocket.reserved)}
-          >
-            Cancel
-          </CancelRocketButton>
-        ) : (
-          <ReserveRocketButton
-            onClick={() => HandleReserveRocket(rocket.id, rocket.reserved)}
-          >
-            Reserve
-          </ReserveRocketButton>
-        )}
+          <p>Reserved</p>
+        ) : null}
+        <div className="img-rocket">
+          <img src={rocket.image} alt={rocket.image} />
+        </div>
+        <button type="button" onClick={() => handleReserveRocket(rocket.id, rocket.reserved)}>
+          {rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+        </button>
       </div>
-      <p>{rocket.description}</p>
-      <div className="img-rocket">
-        <img src={rocket.image} alt={rocket.image} />
-      </div>
-    </div>
-  ));
+    ));
+  };
+
+  return (
+    <section>
+      <h1>Rockets</h1>
+      {renderRockets()}
+    </section>
+  );
 };
-return (
-  <section>
-    <h1>Rockets</h1>
-    {renderRockets(HandleReserveRocket)}
-  </section>
-);
-};
+
 export default Rockets;
