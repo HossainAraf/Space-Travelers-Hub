0dcdbbb22a19626f9721f006f771331fd52cf2e9
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-09-21 12:48:25 +0300

    Update Rockets.js
    updated rockets .js

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,42 +1,26 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { getRockets } from '../redux/rocketsSlice';
-// import '../styles/Rockets.css';
+import { getRockets, reserveRocket, cancelRocket } from '../redux/rocketsSlice';
+import { ReserveRocketButton, CancelRocketButton } from './Button';
+import '../styles/rockets.css';
 
 // GET ROCKETS DATA FROM REDUX STORE
 const Rockets = () => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets.rockets);
-  // console.log('Rockets:', rockets);
+  console.log('Rockets:', rockets);
 
   // FETCH ROCKETS DATA ON PAGE LOAD
   useEffect(() => {
     dispatch(getRockets());
   }, [dispatch]);
 
-  // SHOW LOADING, ERROR, OR ROCKETS DATA
-  const renderRockets = () => {
-    if (rockets.length === 0) {
-      return <p>Loading rockets...</p>;
+  // RESERVE/CANCEL RESERVATION OF ROCKETS
+  const HandleReserveRocket = (id, reserved) => {
+    if (reserved) {
+      dispatch(cancelRocket(id));
+    } else {
+      dispatch(reserveRocket(id));
     }
-
-    return rockets.map((rocket) => (
-      <div className="container-rocket" key={rocket.id}>
-        <h2>{rocket.name}</h2>
-        <p>{rocket.description}</p>
-        <div className="img-rocket">
-          <img src={rocket.image} alt={rocket.image} />
-        </div>
-      </div>
-    ));
+    console.log('HandleReserveRocket:', id, reserved);
   };
-
-  return (
-    <section>
-      <h1>Rockets</h1>
-      {renderRockets()}
-    </section>
-  );
-};
-
-export default Rockets;
