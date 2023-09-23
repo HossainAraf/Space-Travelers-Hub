3b49d972b3c5662671e3e5bd7bba4175b367b2c7
Hossain <arafat.kd99@gmail.com>
2023-09-23 07:50:12 +0100

    Update test files

--- src/__tests__/store.test.js
+++
@@ -1,8 +0,0 @@
-// import { persistor } from '../redux/store';
-
-// describe('Redux Store Configuration', () => {
-//   it('should export a configured Redux store', () => {
-//     // Check if the store object is defined
-//     expect(persistor).toBeDefined();
-//   });
-// });
